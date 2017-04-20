084f810eb012c221c2b32988c43ac1426b2e001d
Jeroen Meulendijks <jeroen.meulendijks@gmail.com>
2017-04-20 22:42:52 +0200

    Cleanup imports

--- src/main/java/nl/meul/kilometerservice/RestInterface.java
+++ src/main/java/nl/meul/kilometerservice/RestInterface.java
@@ -1,13 +1,7 @@
 package nl.meul.kilometerservice;
 
-import com.drew.lang.GeoLocation;
 import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
